70d0e618bf0bde12ca6bba97482e7c309f6fecb0
Pooul14 <alejandra.rodriguezsilva2000@gmail.com>
2025-11-30 16:27:00 +0000

    Currency database store and recorder implemented and tested.

--- src/main/java/software/ulpgc/moneycalculator/application/beatles/Database.java
+++ src/main/java/software/ulpgc/moneycalculator/application/beatles/Database.java
@@ -1,4 +1,106 @@
 package software.ulpgc.moneycalculator.application.beatles;
 
+import software.ulpgc.moneycalculator.architecture.model.Currency;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public class Database {
+
+    public static class CurrencyRecorder implements software.ulpgc.moneycalculator.architecture.io.CurrencyRecorder {
+
+        private final Connection connection;
+        private final PreparedStatement preparedStatement;
+
+        public CurrencyRecorder(Connection connection, String tableName) throws SQLException {
+            this.connection = connection;
+            createTableIfNotExists(tableName);
+            this.preparedStatement = connection.prepareStatement("INSERT INTO " + tableName + " (code, country) VALUES (?, ?)");
+        }
+
+        @Override
+        public void record(Stream<Currency> currencies) {
+            try {
+                currencies.forEach(this::insert);
+                preparedStatement.executeBatch();
+                connection.commit();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void insert(Currency currency) {
+            try {
+                preparedStatement.setString(1, currency.code());
+                preparedStatement.setString(2, currency.country());
+                preparedStatement.addBatch();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void createTableIfNotExists(String tableName) throws SQLException {
+            this.connection.createStatement().execute("CREATE TABLE IF NOT EXISTS " + tableName + " (code TEXT, country TEXT)");
+        }
+    }
+
+    public static class CurrencyStore implements software.ulpgc.moneycalculator.architecture.io.CurrencyStore {
+
+        private final Connection connection;
+        private final String tableName;
+
+        public CurrencyStore(Connection connection, String tableName) {
+            this.connection = connection;
+            this.tableName = tableName;
+        }
+
+        @Override
+        public Stream<Currency> currencies() {
+            try {
+                return currenciesIn(resultSet(tableName));
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private Stream<Currency> currenciesIn(ResultSet resultSet) {
+            return Stream.generate(() -> nextCurrencyIn(resultSet))
+                    .onClose(() -> close(resultSet))
+                    .takeWhile(Objects::nonNull);
+        }
+
+        private Currency nextCurrencyIn(ResultSet resultSet) {
+            try {
+                return resultSet.next() ? readCurrencyIn(resultSet) : null;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private Currency readCurrencyIn(ResultSet resultSet) {
+            try {
+                return new Currency(resultSet.getString(1), resultSet.getString(2));
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void close(ResultSet resultSet) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private ResultSet resultSet(String tableName) throws SQLException {
+            return this.connection.createStatement().executeQuery("SELECT * FROM " + tableName + " ORDER BY code");
+        }
+
+    }
+
 }
--- src/main/java/software/ulpgc/moneycalculator/application/beatles/Main.java
+++ src/main/java/software/ulpgc/moneycalculator/application/beatles/Main.java
@@ -3,11 +3,33 @@ package software.ulpgc.moneycalculator.application.beatles;
 import software.ulpgc.moneycalculator.architecture.control.ExchangeMoneyCommand;
 import software.ulpgc.moneycalculator.architecture.control.HistoricalExchangeMoneyCommand;
 import software.ulpgc.moneycalculator.architecture.control.ViewHistoryCommand;
+import software.ulpgc.moneycalculator.architecture.io.CurrencyStore;
+import software.ulpgc.moneycalculator.architecture.model.Currency;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
-        Desktop desktop = new Desktop(new WebService.CurrencyStore().currencies(),
-                                      new WebService.HistoricalCurrencyStore().currencies());
+
+    private static final String databaseConnectionUrl = "jdbc:sqlite:";
+    private static final String currencyDatabase = "currencies.db";
+
+    private static final String currenciesTable = "currencies";
+    private static final String historicalCurrenciesTable = "historicalCurrencies";
+
+    private static Connection connection;
+
+    public static void main(String[] args) throws SQLException {
+
+        connection = DriverManager.getConnection(databaseConnectionUrl + currencyDatabase);
+        connection.setAutoCommit(false);
+
+        Desktop desktop = new Desktop(currenciesIn(currenciesTable).currencies(),
+                                      currenciesIn(historicalCurrenciesTable).currencies());
+
         desktop.addCommand("exchange", new ExchangeMoneyCommand(
                 desktop.moneyDialog(),
                 desktop.outputCurrencyDialog(),
@@ -31,4 +53,35 @@ public class Main {
         ));
         desktop.setVisible(true);
     }
+
+    private static CurrencyStore currenciesIn(String tableName) throws SQLException {
+        if (tableNonExistent(connection, tableName)) importCurrenciesInto(connection, tableName);
+        return new Database.CurrencyStore(connection, tableName);
+    }
+
+    private static boolean tableNonExistent(Connection connection, String tableName) throws SQLException {
+        return fileIsEmpty(connection) || !connection.getMetaData()
+                .getTables(null, null, tableName, new String[]{"TABLE"})
+                .next();
+    }
+
+    private static boolean fileIsEmpty(Connection connection) throws SQLException {
+        return new File(pathOf(connection)).length() == 0;
+    }
+
+    private static String pathOf(Connection connection) throws SQLException {
+        return pathOf(connection.getMetaData().getURL());
+    }
+
+    private static String pathOf(String url) {
+        return url.substring(url.lastIndexOf(":") + 1);
+    }
+
+    private static void importCurrenciesInto(Connection connection, String tableName) throws SQLException {
+        Stream<Currency> currencies = tableName.equals(currenciesTable) ?
+                new WebService.CurrencyStore().currencies() :
+                new WebService.HistoricalCurrencyStore().currencies();
+        new Database.CurrencyRecorder(connection, tableName).record(currencies);
+    }
+
 }
